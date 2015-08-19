15674ce8a3eebfe176d7fd12be0a5e496ecc02b1
Matthew O'Riordan <matthew.oriordan@gmail.com>
2015-08-19 23:40:34 +0100

    Rename files for clarity
    * controller sets up the app and views
    * app provides app logic for chat
    * view simply allows the view layer to be modified

---
+++ www/js/controller.js
@@ -0,0 +1,92 @@
+/* Set up the view and link in the UI controller and app */
+
+$(document).ready(function () {
+    Origami.fastclick(document.body);
+    var $mainAppView = $('#main-app-view');
+    var $enterNameView = $('#enter-name-view');
+    var $messageText = $('#message-text');
+    var $name = $('#name');
+    var $nameForm = $('#name-form');
+    var $messageForm = $('#message-form');
+    var $membersLozenge = $mainAppView.find('.members-lozenge');
+    var $membersListPopup = $('#members-list-popup');
+    var $dialogCloseButton = $('#dialog-close');
+
+    var uiController = new UiController();
+    var app = new ChatApp(uiController);
+
+    // DO NOT COMMIT THIS
+    window.app = app;
+
+    if (window.isRunningOnMobile) {
+        // Cordova handlers for app pause and app resume
+        // * disconnect when moving to background
+        // * connect back to Ably when moving back to the foreground
+        document.addEventListener('deviceready', function() {
+            document.addEventListener('pause', function () {
+                app.disconnect();
+            });
+
+            document.addEventListener('resume', function () {
+                app.reconnect();
+            });
+        });
+    }
+
+    // Joins the channel using the name (clientId) entered by the user
+    $nameForm.on('submit', function (ev) {
+        ev.preventDefault();
+
+        if ($name.val() === '') {
+            $name.attr('placeholder', 'Name is required').addClass('validation-error')
+            return;
+        }
+
+        $enterNameView.hide();
+        $mainAppView.show();
+
+        uiController.showLoadingOverlay('Connecting to Ably...');
+        app.initialize($name.val());
+    });
+
+    // Sends the message typed by the user and stops the 'user is typing' notification
+    $messageForm.on('submit', function (ev) {
+        ev.preventDefault();
+
+        if ($messageText.val().trim() === '') {
+            return;
+        }
+
+        app.publishMessage($messageText.val());
+        $messageText.val('');
+        $messageText.focus();
+
+        app.sendTypingNotification(false);
+    });
+
+    // Sends a 'user is typing' notification, except when completing a message via the Enter key.
+    $messageText.on('keydown', function (e) {
+        if (e.keyCode == 13) {
+            return;
+        }
+
+        app.sendTypingNotification(true);
+    });
+
+    // Sends a 'user has stopped typing' notification, after 2 seconds have passed since the last keystroke.
+    $messageText.on('keyup', _.debounce(function () {
+        app.sendTypingNotification(false);
+    }, 2000));
+
+    $membersLozenge.on('click', function () {
+        $membersListPopup.show();
+    });
+
+    $dialogCloseButton.on('click', function () {
+        $membersListPopup.hide();
+    });
+
+    window.onunload = window.onbeforeunload = function () {
+        app.disconnect();
+    };
+});
--- www/js/view.js
+++ www/js/view.js
@@ -1,90 +1,188 @@
-/* Set up the view and link in the UI controller and app */
+(function (window) {
+    "use strict";
 
-$(document).ready(function () {
-    Origami.fastclick(document.body);
-    var $mainAppView = $('#main-app-view');
-    var $enterNameView = $('#enter-name-view');
-    var $messageText = $('#message-text');
-    var $name = $('#name');
-    var $nameForm = $('#name-form');
-    var $messageForm = $('#message-form');
-    var $membersLozenge = $mainAppView.find('.members-lozenge');
-    var $membersListPopup = $('#members-list-popup');
-    var $dialogCloseButton = $('#dialog-close');
+    // Constructs an UiController, containing various functions related to the UI:
+    // * Triggering loading overlay
+    // * Displaying presence messages
+    // * Displaying chat messages
+    // * Displaying members count and mentioning
+    function UiController() {
+        var controller = this;
+        var $messageList = $('#message-list');
+        var $loadingOverlay = $('#loading-overlay');
+        var $loadingMessage = $('#loading-message');
+        var $loadingHistory = $('#loading-history');
+        var $messageFormInputs = $('#message-form :input');
+        var $connectionInfo = $('#connection-info');
+        var $messageText = $('#message-text');
+        var $membersCountLozenge = $('#main-app-view').find('.members-count');
+        var $membersTypingNotification = $('#members-typing-indication');
+        var $membersList = $('#members-list');
+        var $membersListPopup = $('#members-list-popup');
 
-    var uiController = new UiController();
-    var app = new ChatApp(uiController);
+        this.showLoadingOverlay = function(message) {
+            $loadingMessage.text(message || 'Loading...');
+            $loadingOverlay.show();
+        }
 
+        this.hideLoadingOverlay = function() {
+            $loadingOverlay.hide();
+        }
 
-    if (window.isRunningOnMobile) {
-        // Cordova handlers for app pause and app resume
-        // * disconnect when moving to background
-        // * connect back to Ably when moving back to the foreground
-        document.addEventListener('deviceready', function() {
-            document.addEventListener('pause', function () {
-                app.disconnect();
+        this.showLoadingHistory = function() {
+            $loadingHistory.show();
+        }
+
+        this.hideLoadingHistory = function() {
+            $loadingHistory.hide();
+        }
+
+        function publishedFromSelf(message) {
+            return message.clientId == controller.clientId;
+        }
+
+        function messageElem(message) {
+            var dateAsLocalTime = Utils.formatDateAsLocalTime(new Date(message.timestamp));
+            var $author = $('<span class="author">').text(message.clientId);
+            var $time = $('<div class="time">').text(dateAsLocalTime);
+            var $message = $('<div class="message">').text(message.data);
+            var $back = $('<div class="back">');
+            var $li = $('<div class="message-bubble">');
+
+            $back.append($author, $time, $message);
+            $li.append($back);
+
+            if (publishedFromSelf(message)) {
+                return $li.addClass('sent');
+            } else {
+                return $li.addClass('received');
+            }
+        }
+
+        function presenceElem(presence) {
+            var actionText, $text, $div;
+
+            if (presence.action === Ably.Realtime.PresenceMessage.Action.ENTER) {
+                actionText = 'entered';
+            } else if (presence.action === Ably.Realtime.PresenceMessage.Action.LEAVE) {
+                actionText = 'left';
+            } else {
+                return;
+            }
+
+            $text = $('<span class="text">');
+            if (publishedFromSelf(presence)) {
+                $text.text('you ' + actionText + ' the channel');
+            } else {
+                $text.text(presence.clientId + ' has ' + actionText + ' the channel');
+            }
+
+            return $('<div class="message-presence">').append($text);
+        }
+
+        function addToMessageList($elem, historicalMessages) {
+            if (!$elem) { return; } // ignore empty elements i.e. presence messages we won't display such as updates
+
+            // If near the bottom, then scroll new message into focus automatically
+            var shouldScrollAutomatically = $(window).scrollTop() + $(window).height() >= $(document).height() - 100;
+
+            if (historicalMessages) {
+                $messageList.prepend($elem);
+            } else {
+                $messageList.append($elem);
+            }
+
+            if (shouldScrollAutomatically) { window.setTimeout(scrollToBottom, 10); }
+        }
+
+        function scrollToBottom() {
+            $('html, body').scrollTop($(document).height() - $(window).height());
+        }
+
+        // Updates the members count in the lozenge and their names in the 'Members' popup dialog
+        function updateMembers(members) {
+            members = members || [];
+
+            // Typing members are users who have 'isTyping' set to true in their presence data
+            var typingMembersNames = members.filter(function (member) {
+                return member && member.data && member.data.isTyping;
+            }).map(function (member) {
+                return member.clientId;
             });
+            var text = Utils.formatTypingNotification(typingMembersNames);
 
-            document.addEventListener('resume', function () {
-                app.reconnect();
-            });
-        });
+            $membersCountLozenge.text(members.length);
+            $membersTypingNotification.text(text);
+
+            // Clear the member list and replace it with a list with their names
+            $membersList.html('').append(members.map(function (member) {
+                var memberName = member.clientId;
+                var $li = $('<li><a href="javascript:void(0)">').text(memberName);
+                $li.on('click', function () {
+                    $messageText.val(leftTrim($messageText.val() + ' @' + memberName + ' '));
+                    $membersListPopup.hide();
+                    $messageText.focus();
+                });
+
+                return $li;
+            }));
+        }
+
+        // Connection change handler
+        // * Disconnected: disable user input and display meaningful message
+        // * Connected: reenable input and hide message
+        this.onConnectionChange = function(state) {
+            if (state.current === 'disconnected' || state.current === 'suspended') {
+                $messageFormInputs.prop('disabled', true);
+                $connectionInfo.text(state.reason.message);
+            } else if (state.current === 'connected') {
+                $messageFormInputs.prop('disabled', false);
+                $connectionInfo.text('');
+            }
+        }
+
+        // Receives an Ably message and shows it on the screen
+        this.onMessageReceived = function (message) {
+            addToMessageList(messageElem(message));
+        };
+
+        // Receives an Ably presence message and shows it on the screen
+        this.onPresenceReceived = function (presenceMessage, members) {
+            addToMessageList(presenceElem(presenceMessage));
+            updateMembers(members);
+        };
+
+        this.addHistoricalMessages = function (messages) {
+            var message, elem;
+            for (var i = 0; i < messages.length; i++) {
+                message = messages[i]
+                if (message.action) {
+                    elem = presenceElem(message);
+                } else {
+                    elem = messageElem(message);
+                }
+                addToMessageList(elem, true);
+            }
+        }
+
+        // Generic error handler - alert()s an error, if one exists
+        this.onError = function (err) {
+            var errorMessage = "Oops, something has gone wrong. We recommend you restart this demo.";
+            if (err) {
+                if (err.message) {
+                    errorMessage += "\n" + err.message;
+                } else {
+                    errorMessage += "\n" + JSON.stringify(err);
+                }
+            }
+            controller.hideLoadingOverlay();
+        };
+
+        // Clears the messages list
+        this.resetMessages = function () {
+            $messageList.empty();
+        };
     }
 
-    // Joins the channel using the name (clientId) entered by the user
-    $nameForm.on('submit', function (ev) {
-        ev.preventDefault();
-
-        if ($name.val() === '') {
-            $name.attr('placeholder', 'Name is required').addClass('validation-error')
-            return;
-        }
-
-        $enterNameView.hide();
-        $mainAppView.show();
-
-        uiController.showLoadingOverlay('Connecting to Ably...');
-        app.initialize($name.val());
-    });
-
-    // Sends the message typed by the user and stops the 'user is typing' notification
-    $messageForm.on('submit', function (ev) {
-        ev.preventDefault();
-
-        if ($messageText.val().trim() === '') {
-            return;
-        }
-
-        app.publishMessage($messageText.val());
-        $messageText.val('');
-        $messageText.focus();
-
-        app.sendTypingNotification(false);
-    });
-
-    // Sends a 'user is typing' notification, except when completing a message via the Enter key.
-    $messageText.on('keydown', function (e) {
-        if (e.keyCode == 13) {
-            return;
-        }
-
-        app.sendTypingNotification(true);
-    });
-
-    // Sends a 'user has stopped typing' notification, after 2 seconds have passed since the last keystroke.
-    $messageText.on('keyup', _.debounce(function () {
-        app.sendTypingNotification(false);
-    }, 2000));
-
-    $membersLozenge.on('click', function () {
-        $membersListPopup.show();
-    });
-
-    $dialogCloseButton.on('click', function () {
-        $membersListPopup.hide();
-    });
-
-    window.onunload = window.onbeforeunload = function () {
-        app.disconnect();
-    };
-});
+    window.UiController = UiController;
+}(window));
